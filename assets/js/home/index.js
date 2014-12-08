060ad9e4ecfe62e80a21519f40f1c8bf2972600e
pvkhoi <pvkhoi@apcs.vn>
2014-12-09 01:00:28 +0700

    continue to design homepage

--- assets/js/home/index.js
+++ assets/js/home/index.js
@@ -2,10 +2,18 @@
 
 	var config = {
 		$panels : $('.home-panel'),
+		$fastfoodSection: $('#fastfood-section'),
+		$maindishSection: $('#maindish-section'),
+		$drinksSection: $('#drinks-section'),
+		$dessertSection: $('#dessert-section')
 	};
 
 	var init = function() {
 		config.$panels = $('.home-panel');
+		$fastfoodSection = $('#fastfood-section');
+		$maindishSection = $('#maindish-section');
+		$drinksSection = $('#drinks-section');
+		$dessertSection = $('#dessert-section');
 	};
 
 	var setupHeight = function() {
@@ -20,20 +28,40 @@
 
 
 	var bind = function() {
-
 		$(document).ready(function() {
 			init();
 			setupHeight();
 		});
-
 		$(window).load(function() {
 			
 		});
-
 		$(window).on('resize', Foundation.utils.throttle(function(e){
 		 	setupHeight();
 		}, 300));
-
+		$('#nav-fastfood').click(function() {
+			$('html,body').animate({
+				scrollTop: config.$fastfoodSection.offset().top
+			}, 1000);
+			return false;
+		});
+		$('#nav-maindish').click(function() {
+			$('html,body').animate({
+				scrollTop: config.$maindishSection.offset().top
+			}, 1000);
+			return false;
+		});
+		$('#nav-drinks').click(function() {
+			$('html,body').animate({
+				scrollTop: config.$drinksSection.offset().top
+			}, 1000);
+			return false;
+		});
+		$('#nav-dessert').click(function() {
+			$('html,body').animate({
+				scrollTop: config.$dessertSection.offset().top
+			}, 1000);
+			return false;
+		});
 	};
 
 	bind();
